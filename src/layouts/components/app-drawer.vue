c5347c8bd53400af1785c32d38899ea8595f9ab9
Min <solunar@vip.qq.com>
2023-12-20 21:02:09 +0800

    Update

---
+++ src/layouts/components/app-drawer.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <q-list>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon color="primary" name="bluetooth" />
+      </q-item-section>
+      <q-item-section>Icon as avatar</q-item-section>
+    </q-item>
+  </q-list>
+</template>
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -2,28 +2,32 @@
 import { ref } from 'vue';
 import { useQuasar } from 'quasar';
 
+import AppDrawer from './components/app-drawer.vue';
+
 defineOptions({ name: 'app-layout' });
 
 const $q = useQuasar();
 $q.dark.set(true);
 
 const drawer = ref(true);
-const toggle = () => {
-  drawer.value = !drawer.value;
-};
-
 const tweak = (offset: number, height: number) => ({ height: `${height - offset}px` });
 </script>
 
 <template>
   <q-layout view="hhh lpr fff" class="overflow-hidden">
-    <q-drawer v-model="drawer" :width="260"></q-drawer>
+    <q-drawer v-model="drawer" :width="260">
+      <app-drawer />
+    </q-drawer>
     <q-page-container>
       <q-page :style-fn="tweak" class="overflow-auto relative">
-        <div @click="toggle" id="target" class="flex justify-center items-center absolute left-0 top-1/2 -translate-y-1/2 w-8 h-16 cursor-pointer">
+        <div
+          @click="drawer = !drawer"
+          id="target"
+          class="flex justify-center items-center absolute left-0 top-1/2 -translate-y-1/2 w-8 h-16 cursor-pointer"
+        >
           <b class="w-1 h-5 bg-gray-400 rounded-sm"></b>
           <q-tooltip target="#target" anchor="center right" self="center left" transition-show="fade" transition-hide="fade" class="bg-black">
-            {{ drawer ? '收起' : '展开' }}侧边栏
+            <span>{{ drawer ? '收起' : '展开' }}侧边栏</span>
           </q-tooltip>
         </div>
         <router-view v-slot="{ Component }">
