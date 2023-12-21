701b2f76c3c5ffa1a7e52677f56f38f95b1eecf9
Min <solunar@vip.qq.com>
2023-12-21 14:43:15 +0800

    Update

--- src/layouts/components/app-drawer.vue
+++ src/layouts/components/app-drawer.vue
@@ -1,12 +1,21 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { examplesApi } from '@/apis/examples';
 
+import DialogDelete from './dialog-delete.vue';
 import DialogSettings from './dialog-settings.vue';
 
 const chats = ref();
+const chat = ref();
+const deleteChat = (c: any) => {
+  dialog.delete = true;
+  chat.value = c;
+};
 
-const settings = ref(false);
+const dialog = reactive({
+  delete: false,
+  settings: false
+});
 
 onMounted(() => {
   examplesApi
@@ -41,10 +50,21 @@ onMounted(() => {
     </q-list>
 
     <q-list class="flex-1 space-y-2">
-      <q-item v-for="chat in chats" :key="chat.id.value" class="rounded py-0 px-2 min-h-[32px]" clickable v-ripple>
-        <q-item-section>{{ chat.name.title }} {{ chat.name.first }} {{ chat.name.last }}</q-item-section>
+      <q-item v-for="c in chats" :key="c.id.value" class="rounded py-0 px-2 min-h-[32px]" clickable v-ripple>
+        <q-item-section>{{ c.name.title }} {{ c.name.first }} {{ c.name.last }}</q-item-section>
         <q-item-section side>
-          <q-icon name="more_horiz" size="xs" />
+          <q-icon name="more_horiz" size="xs">
+            <q-menu :class="{ 'bg-zinc-900': $q.dark.isActive }" class="w-40">
+              <q-list>
+                <q-item @click="deleteChat(c)" class="text-red-500" clickable v-close-popup>
+                  <q-item-section avatar>
+                    <q-icon name="delete" size="xs" />
+                  </q-item-section>
+                  <q-item-section>删除会话</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-icon>
         </q-item-section>
       </q-item>
     </q-list>
@@ -60,7 +80,7 @@ onMounted(() => {
 
         <q-menu :offset="[0, 8]" :class="{ 'bg-zinc-900': $q.dark.isActive }" fit>
           <q-list>
-            <q-item @click="settings = true" clickable v-close-popup>
+            <q-item @click="dialog.settings = true" clickable v-close-popup>
               <q-item-section avatar>
                 <q-icon name="settings" size="xs" />
               </q-item-section>
@@ -72,5 +92,6 @@ onMounted(() => {
     </q-list>
   </div>
 
-  <dialog-settings v-model="settings" />
+  <dialog-delete v-model="dialog.delete" :chat="chat" />
+  <dialog-settings v-model="dialog.settings" />
 </template>
---
+++ src/layouts/components/dialog-delete.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{ chat: any }>();
+const emit = defineEmits(['update:model-value']);
+
+const cancel = (): void => emit('update:model-value', false);
+</script>
+
+<template>
+  <q-dialog v-bind="$attrs" @hide="cancel">
+    <q-card class="w-96">
+      <div class="flex justify-between items-center py-2 px-4">
+        <div class="text-base">删除会话？</div>
+        <q-btn icon="close" dense flat round v-close-popup />
+      </div>
+      <q-separator />
+      <div>{{ props.chat.id.value }}</div>
+    </q-card>
+  </q-dialog>
+</template>
